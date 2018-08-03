2c4eb2e0ddb1d8d9776d87b5242a7514d8a4f0e5
wangyuxin <wangyuxin@cashbus.com>
2018-08-03 18:17:02 +0800

    first commit

---
+++ src/main/java/com/faderw/DwMapperApplication.java
@@ -0,0 +1,12 @@
+package com.faderw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DwMapperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DwMapperApplication.class, args);
+	}
+}
---
+++ src/test/java/com/faderw/DwMapperApplicationTests.java
@@ -0,0 +1,16 @@
+package com.faderw;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DwMapperApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
